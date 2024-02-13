54b538a7943fa4174605d952a72af70763aeef69
judejay <judejay26@gmail.com>
2024-02-13 16:53:27 +0000

    feat: scripts tag

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,20 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>;
+      <Head>
+        <title>First Post</title>;
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
